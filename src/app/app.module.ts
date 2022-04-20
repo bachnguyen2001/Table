d087735f4ef0fe16e1de5ab7e372f32c4e0bbc1a
Najumy <najumy1807.2k@gmail.com>
2022-04-20 22:45:23 +0700

    Add new file DuyPn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BirthdayPipe } from './birthday.pipe';
 import { PhonePipe } from './phone.pipe';
 import { FormsModule } from '@angular/forms';
+import { LineChartComponent } from './region/line-chart/line-chart.component';
+import { MapComponent } from './region/map/map.component';
+import { RegionalListComponent } from './region/regional-list/regional-list.component';
+import { WorldwideComponent } from './worldwide/worldwide.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BirthdayPipe,
-    PhonePipe
+    PhonePipe,
+    LineChartComponent,
+    MapComponent,
+    RegionalListComponent,
+    WorldwideComponent
   ],
   imports: [
     BrowserModule,
